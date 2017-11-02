970964bda4cf247eb5077216347f45c2e879191b
Raymond Hong <rhong612@gmail.com>
2017-11-01 17:16:09 -0700

    Registration now directs to login page upon success

--- flaskfrontend/app/views.py
+++ flaskfrontend/app/views.py
@@ -69,7 +69,8 @@ def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User()
-        db.add(user)
+        user.email = request.form.get('email')
+        db.session.add(user)
         flash('Thanks for registering')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
