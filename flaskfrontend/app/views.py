33e803a330fc1761c6e52589e767dbf16e4bde46
Raymond Hong <rhong612@gmail.com>
2017-11-02 11:16:09 -0700

    User accounts are now stored in db

--- flaskfrontend/app/views.py
+++ flaskfrontend/app/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm
+from app import app, db, lm, models
 from config import ALLOWED_EXTENSIONS
 from .models import User
 from .forms import LoginForm, RegistrationForm, EditForm
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash
 
 
 @app.before_request
@@ -73,12 +74,12 @@ def errorcase():
 def login():
     # if g.user is not None and g.user.is_authenticated:
     #    return redirect(url_for('upload'))
-    form = LoginForm()
+    form = LoginForm(request.form)
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = models.User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(request.args.get('next') or url_for('index'))
         flash('Invalid username or password.')
         session['remember_me'] = form.remember_me.data
     return render_template('login.html',
@@ -92,8 +93,9 @@ def register():
     if request.method == 'POST' and form.validate():
         user = User()
         user.email = request.form.get('email')
-        user.password_hash = request.form.get('password')  # will hash later
+        user.password_hash = generate_password_hash(request.form.get('password'))
         db.session.add(user)
+        db.session.commit()
         flash('Thanks for registering')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
