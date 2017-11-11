1be4556349f1b175e8f88965ef18e4a9ffcaa834
Raymond Hong <rhong612@gmail.com>
2017-11-11 12:07:32 -0800

    Removed unused import

--- flaskfrontend/app/views.py
+++ flaskfrontend/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, session, url_for, request, g, send_from_directory, Markup
+from flask import render_template, flash, redirect, session, url_for, request, g, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, models
 from .models import User
