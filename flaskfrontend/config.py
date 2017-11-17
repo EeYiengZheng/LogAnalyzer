2ff985b66e0a194459148cdc9135bc87b95ef9ad
Ee Yieng Zheng <eeyiengz@hotmail.com>
2017-11-17 00:28:32 -0800

    moved secret_key to a new file

--- flaskfrontend/config.py
+++ flaskfrontend/config.py
@@ -1,8 +1,10 @@
 import os
+from secret import secret_key
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = 'tree-boot-info-percent-define'
+SECRET_KEY = secret_key
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
