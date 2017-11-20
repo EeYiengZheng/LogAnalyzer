df2fe6b94153a8ca35c709814672ac63d4f27632
Ze Martinho <zeymartinho@gmail.com>
2017-11-20 08:19:24 -0800

    ok now fixed??

---
+++ flaskfrontend/config.py
@@ -0,0 +1,16 @@
+import os
+#from secret import secret_key
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+#SECRET_KEY = secret_key
+SECRET_KEY = 'you-will-never-guess'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+ALLOWED_EXTENSIONS = ['log']  # <-- don't know where to put this? config.py
+UPLOAD_FOLDER = 'uploads'
+ANALYZED_CSV_FOLDER = 'analyzed'
