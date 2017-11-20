af0c0a868709298a6d0f72c43f1e41e4abd93d7c
Ze Martinho <zeymartinho@gmail.com>
2017-11-20 10:12:53 -0800

    added back secret to config.py

--- flaskfrontend/config.py
+++ flaskfrontend/config.py
@@ -1,11 +1,10 @@
 import os
-#from secret import secret_key
+from secret import secret_key
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-#SECRET_KEY = secret_key
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = secret_key
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
